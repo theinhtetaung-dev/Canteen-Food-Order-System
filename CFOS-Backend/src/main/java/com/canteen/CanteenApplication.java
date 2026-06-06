2a8ae11e01e701c608d512f7a170713963699df5
theinhtetaung-dev <theinhtetaung.developer@gmail.com>
2026-06-06 23:30:47 +0630

    chore: remove unnecessary blank line

--- CFOS-Backend/src/main/java/com/canteen/CanteenApplication.java
+++ CFOS-Backend/src/main/java/com/canteen/CanteenApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CanteenApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CanteenApplication.class, args);
 	}
